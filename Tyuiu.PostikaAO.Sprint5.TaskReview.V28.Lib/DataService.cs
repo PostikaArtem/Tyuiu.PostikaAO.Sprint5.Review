341a24d9d1c283f7ce5c722dd31688559b6646e5
PostikaArtem <Tyuiu.PostikaAO@outlook.com>
2023-11-28 14:27:49 +0500

    Add project files.

---
+++ Tyuiu.PostikaAO.Sprint5.TaskReview.V28.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PostikaAO.Sprint5.TaskReview.V28.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tyuiu.PostikaAO.Sprint5.TaskReview.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PostikaAO.Sprint5.TaskReview.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PostikaAO.Sprint5.TaskReview.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26117c54-c8f4-47e8-9c1b-c759a8761860")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
